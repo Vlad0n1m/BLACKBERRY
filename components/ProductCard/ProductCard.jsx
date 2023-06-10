aa8af73fee4288ce8cc5f213d383b6aa766f3fd7
Vlad0n1m <VladKarachkov321@gmail.com>
2023-06-10 23:40:52 +0600

    products card

--- components/ProductCard/ProductCard.jsx
+++ components/ProductCard/ProductCard.jsx
@@ -1,12 +1,12 @@
 export default function ProductCard() {
     return (
-      <div className="flex-col flex gap-[8px] text-white">
-        <div className="flex gap-[8px]">
-          <div className="flex flex-col w-full gap-[10px]">
-            <h1 className="flex justify-center items-center bg-center w-[100%] text-[32px] aspect-square rounded-lg bg-[url('/images/coffe.webp')]">
+      <div className="flex-col md:flex-row flex gap-[8px] md:gap-[20px] md:justify-between text-white">
+        <div className="w-full flex gap-[8px] md:gap-[20px] md:justify-between ">
+          <div className="flex flex-col w-full gap-[10px]  ">
+            <h1 className="flex justify-center items-center bg-no-repeat bg-center w-[100%] text-[32px] aspect-square rounded-lg bg-[url('/images/coffe.webp')]">
               café
             </h1>
-            <div className="w-full bg-[#27272a] flex flex-col gap-[4px] rounded-lg  p-[12px]">
+            <div className="w-full bg-[#27272a] flex flex-col gap-[4px] rounded-lg p-[12px]">
               <h1 className="text-[18px] font-[600]">Кофе</h1>
               <p className="text-[12px] font-[400] opacity-100">
                 Делаем все, чтобы ваш день был лучше!
@@ -14,7 +14,7 @@ export default function ProductCard() {
             </div>
           </div>
           <div className="flex flex-col w-full gap-[10px]">
-            <h1 className="flex justify-center items-center bg-center w-[100%] text-[32px] aspect-square rounded-lg bg-[url('/images/cruasan.webp')]">
+            <h1 className="flex justify-center items-center bg-no-repeat bg-center object-fill w-full text-[32px] aspect-square rounded-lg bg-[url('/images/cruasan.webp')]">
               croissant
             </h1>
             <div className="w-full bg-[#27272a] flex flex-col gap-[4px] rounded-lg  p-[12px]">
@@ -25,9 +25,9 @@ export default function ProductCard() {
             </div>
           </div>
         </div>
-        <div className="flex gap-[8px]">
+        <div className="flex w-full  md:justify-between gap-[8px] md:gap-[20px]">
           <div className="flex flex-col w-full gap-[10px]">
-            <h1 className="flex justify-center items-center bg-center w-[100%] text-[32px] aspect-square rounded-lg bg-[url('/images/lunch.webp')]">
+            <h1 className="flex justify-center items-center bg-no-repeat bg-center w-[100%] text-[32px] aspect-square rounded-lg bg-[url('/images/lunch.webp')]">
               nourriture
             </h1>
             <div className="w-full bg-[#27272a] flex flex-col gap-[4px] rounded-lg  p-[12px]">
@@ -38,7 +38,7 @@ export default function ProductCard() {
             </div>
           </div>
           <div className="flex flex-col w-full gap-[10px]">
-            <h1 className="flex justify-center items-center bg-center w-[100%] text-[32px] aspect-square rounded-lg bg-[url('/images/boison.webp')]">
+            <h1 className="flex justify-center items-center bg-no-repeat bg-center w-[100%] text-[32px] aspect-square rounded-lg bg-[url('/images/boison.webp')]">
               boisson
             </h1>
             <div className="w-full bg-[#27272a] flex flex-col gap-[4px] rounded-lg  p-[12px]">
