3365163a180d31ff5a6236ea403dc26ff059fb92
Vlad0n1m <VladKarachkov321@gmail.com>
2023-07-20 15:23:18 +0600

    deploy

--- components/Activities/Activivties.jsx
+++ components/Activities/Activivties.jsx
@@ -20,12 +20,12 @@ export default function Activivties() {
       <div className="flex items-center justify-between gap-[10px]">
         <Link
           
-          href="/franchise"
+          href="/"
           id="shadow-down-brown text-shadow"
           className="cursor-pointer flex grow-[1] sm:grow-[2] justify-start text-[16px] sm:text-[33px] w-[90px] sm:w-[unset] md:text-[50px] font-[600] leading-[100%] items-end bg-[url('/images/franshisa.webp')] h-full  rounded-lg  max-h-[90px] sm:max-h-[300px] aspect-square sm:aspect-[unset] sm:min-h-[300px] bg-center bg-cover p-[5px] sm:p-[12px]"
         >
           наша <br />
-          франшиза
+          история
         </Link>
         <Link
           href="/coop"
